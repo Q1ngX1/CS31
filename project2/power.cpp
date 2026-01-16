a9cd9abc07e6ae817d5760332fb13cf0321c2ecb
Bryan <zanshuhan2025@163.com>
2026-01-16 12:20:41 -0800

    20260116

---
+++ project2/power.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /*
+     int x{ 5 };
+     int i, j, k;
+     cout << "Enter:";
+     cin >> i >> j >> k;
+      cout << "Your value of i was:";
+      cout << i << endl;
+      cout << "Your value of j was:";
+      cout << j << endl;
+      cout << "Your value of k was:";
+      cout << k << endl;
+      cout << endl;
+     */
+    cout<<"Start"<<endl;
+    const double PI = 3.1415926;
+    cout<<PI;
+    cerr<<"error";
+    return (0);
+}
