94967dfc83b6b252dad62cf3d2e7ab947ceb23ce
Q1ngX1 <zanshuhan2025@163.com>
2026-01-22 16:52:45 -0800

    20260122

--- test/main.cpp
+++ test/main.cpp
@@ -1,24 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    /*
-     int x{ 5 };
-     int i, j, k;
-     cout << "Enter:";
-     cin >> i >> j >> k;
-      cout << "Your value of i was:";
-      cout << i << endl;
-      cout << "Your value of j was:";
-      cout << j << endl;
-      cout << "Your value of k was:";
-      cout << k << endl;
-      cout << endl;
-     */
-    cout<<"Start"<<endl;
-    const double PI = 3.1415926;
-    cout<<PI;
-    cerr<<"error";
+    double x = 0;
+    cin >> x;
+    cout << x;
     return (0);
 }
